10fe48edaff0ef04ee1d0b72cae3a57f620ca507
ErnestoUlisesHM <netito.uli@icloud.com>
2025-09-20 22:16:18 -0600

    iris supervised and n supervised complete

--- scripts/nsupervised/iris.py
+++ scripts/nsupervised/iris.py
@@ -17,10 +17,11 @@ from sklearn.neighbors import KNeighborsRegressor
 from sklearn.linear_model import LinearRegression
 from sklearn.neural_network import MLPRegressor
 
-from sklearn.preprocessing import LabelEncoder
-
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, r2_score
+
+# We are going to predict without the last column
+from sklearn.cluster import KMeans # For K-Means clustering
 
 # Iris Dataset
 iris = pd.read_csv("data/iris/iris.data", header = None)
@@ -32,28 +33,97 @@ print(iris.head()) # check it
 X = iris.iloc[:, :-1]  # features
 #y = iris.iloc[:, -1]   # target.  We are not going to use it
 
-# this is a 80 - 20 split
-X_train, X_test = train_test_split(X, test_size=0.2, random_state=42)
+# We know there are 3 species, so we set n_clusters = 3
+kmeans = KMeans(n_clusters=3, random_state=42)
+clusters = kmeans.fit_predict(X)
 
-# Because we are going to work with not supervised learning, we are going to predict and not use the last column
+# this is a 70 - 30 split
+# Use clusters as pseudo-targets
+X_train, X_test, y_train, y_test = train_test_split(X, clusters, test_size=0.3, random_state=42)
+
+plt.figure(figsize=(12, 8))
+
 ###################################################################################################
 #                   Random Forest Regressor
-RandomForestRegressorModel = RandomForestRegressor()
-RandomForestRegressorModel.fit(X_train)
+RandomForestRegressorModel = RandomForestRegressor() # choose the regressor
+RandomForestRegressorModel.fit(X_train, y_train) # train the model
+y_pred_rf = RandomForestRegressorModel.predict(X_test) # predict
+
+# Evaluate the model
+print("\nRandom Forest Regressor")
+print("Mean Squared Error:", mean_squared_error(y_test, y_pred_rf))
+print("R^2 Score:", r2_score(y_test, y_pred_rf))
+
+# Plot the results
+plt.subplot(2, 2, 1)
+plt.scatter(y_test, y_pred_rf, alpha=0.7)
+plt.plot([y_test.min(), y_test.max()], [y_test.min(), y_test.max()], 'r--') # 45 degree line to visualize performance
+plt.xlabel("True Values")
+plt.ylabel("Predictions")
+plt.title("Random Forest")
 
 ###################################################################################################
 #                   K Neighbors Regressor
-KNeighborsRegressorModel = KNeighborsRegressor()
-KNeighborsRegressorModel.fit(X_train)
+KNeighborsRegressorModel = KNeighborsRegressor() # choose the regressor
+KNeighborsRegressorModel.fit(X_train, y_train) # train the model
+y_pred_knn = KNeighborsRegressorModel.predict(X_test) # predict
+
+# Evaluate the model
+print("\nK Neighbors Regressor")
+print("Mean Squared Error:", mean_squared_error(y_test, y_pred_knn))
+print("R^2 Score:", r2_score(y_test, y_pred_knn))
+
+# Plot the results
+plt.subplot(2, 2, 2)
+plt.scatter(y_test, y_pred_knn, alpha=0.7)
+plt.plot([y_test.min(), y_test.max()], [y_test.min(), y_test.max()], 'r--') # 45 degree line to visualize performance
+plt.xlabel("True Values")
+plt.ylabel("Predictions")
+plt.title("KNeighbors")
 
 ###################################################################################################
 #                   Linear Regression
-LinearRegressionModel = LinearRegression()
-LinearRegressionModel.fit(X_train)
+LinearRegressionModel = LinearRegression() # choose the regressor
+LinearRegressionModel.fit(X_train, y_train) # train the model
+y_pred_lr = LinearRegressionModel.predict(X_test) # predict
+
+# Evaluate the model
+print("\nLinear Regression")
+print("Mean Squared Error:", mean_squared_error(y_test, y_pred_lr))
+print("R^2 Score:", r2_score(y_test, y_pred_lr))
+
+# Plot the results
+plt.subplot(2, 2, 3)
+plt.scatter(y_test, y_pred_lr, alpha=0.7)
+plt.plot([y_test.min(), y_test.max()], [y_test.min(), y_test.max()], 'r--') # 45 degree line to visualize performance
+plt.xlabel("True Values")
+plt.ylabel("Predictions")
+plt.title("Linear Regression")
 
 ###################################################################################################
 #                   MLP Regressor
 MLPRegressorModel = MLPRegressor()
-MLPRegressorModel.fit(X_train)
+MLPRegressorModel.fit(X_train, y_train)
+y_pred_mlp = MLPRegressorModel.predict(X_test)
+
+# Evaluate the model
+print("\nMLP Regressor")
+print("Mean Squared Error:", mean_squared_error(y_test, y_pred_mlp))
+print("R^2 Score:", r2_score(y_test, y_pred_mlp))
+
+# Plot the results
+plt.subplot(2, 2, 4)
+plt.scatter(y_test, y_pred_mlp, alpha=0.7)
+plt.plot([y_test.min(), y_test.max()], [y_test.min(), y_test.max()], 'r--') # 45 degree line to visualize performance
+plt.xlabel("True Values")
+plt.ylabel("Predictions")
+plt.title("MLP Regressor")
 
 ###################################################################################################
+
+plt.tight_layout()
+plt.savefig("results/iris_nsupervised_regressors_comparison.png") # Save the figure
+plt.show()
+
+# Command to run the script and save the terminal output
+#python scripts/nsupervised/iris.py | tee results/iris_nsupervised_terminal_output.txt
